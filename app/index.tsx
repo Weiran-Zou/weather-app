a22e22f04d57dbe1276cbf44c601e328886134f3
Weiran Zou <zouweiran9122@gmail.com>
2024-11-11 08:46:55 +1100

    Add place searchBar with Google Places autocomplete feature

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,31 @@
-import { Text, View, StyleSheet } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Feather from '@expo/vector-icons/Feather';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_PLACES_API_KEY} from '@env'
+import 'react-native-get-random-values'
 
 export default function Index() {
+
   return (
     <View style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={styles.searchBar}>
+        <Feather name="search" size={24} color="black" style={styles.searchIcon}/>
+        <GooglePlacesAutocomplete
+          GooglePlacesDetailsQuery={{ fields: "geometry" }}
+          fetchDetails={true}
+          placeholder="Search the place"
+          query={{
+            key: GOOGLE_PLACES_API_KEY,
+            language: 'en',
+          }}
+          onPress={(data, details) => {
+            console.log(data.description)
+            console.log(details?.geometry?.location);
+          }}
+          onFail={(error) => console.error(error)}
+        />
+      </View>
     </View>
   );
 }
@@ -11,7 +33,19 @@ export default function Index() {
 const styles  = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
+    padding: 10,
+    backgroundColor: '#efeff1',
+    alignItems:"center"
+  },
+  searchBar: {
+    width: "90%",
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    marginTop: 30
+  },
+  searchIcon: {
+    padding: 10,
+  },
+ 
 })
